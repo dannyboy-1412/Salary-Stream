688781e0fa3037a7983e79ff0f9d0d6f87bbb36a
Daniel <dannyrodrigo14@gmail.com>
2022-06-05 10:49:08 +0530

    fixed bootstrap issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import '../styles/globals.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+    ) 
 }
 
 export default MyApp
